c90e2e32cb33e04292953a70d391c7d52d0f5f0f
Paulo Ferrari <paulo.ferrari@involves.com>
2022-08-01 14:44:40 -0300

    export normalize

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const BaseFilterParameters = require('./src/base-filter-parameters')
 const BaseParameters = require('./src/base-parameters')
+const Normalize = require('./src/normalize')
 const Validation = require('./src/validation')
 
 module.exports = {
   BaseFilterParameters,
   BaseParameters,
+  Normalize,
   Validation
 }
